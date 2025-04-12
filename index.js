95f828ee34af1d9d0f43102085decd683506ee02
Jadzia-Serfoss <123050136+Jadzia-Serfoss@users.noreply.github.com>
2025-04-12 00:17:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const playerDisplay = document.querySelector('.player-display');
+const computerDisplay = document.querySelector('.computer-display');
+const playerScoreDisplay = document.querySelector('.player-score');
+const computerScoreDisplay = document.querySelector('.computer-score');
+const resultDisplay = document.querySelector('.results');
+
+let playerScore = '0';
+let computerScore = '0';
+
+const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+
+function game(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 5)];
+
+  const getResult = (playerChoice, computerChoice) => 
+  
+  playerChoice === computerChoice ? "It's a tie!" : 
+  (playerChoice === 'Scissors' && computerChoice === 'Paper') || 
+  (playerChoice === 'Paper' && computerChoice === 'Rock') || 
+  (playerChoice === 'Rock' && computerChoice === 'Lizard') || 
+  (playerChoice === 'Lizard' && computerChoice === 'Spock') || 
+  (playerChoice === 'Spock' && computerChoice === 'Scissor') || 
+  (playerChoice === 'Scissors' && computerChoice === 'Lizard') ||
+  (playerChoice === 'Lizard' && computerChoice === 'Paper') || 
+  (playerChoice === 'Paper' && computerChoice === 'Spock') || 
+  (playerChoice === 'Spock' && computerChoice === 'Rock') || 
+  (playerChoice === 'Rock' && computerChoice === 'Scissors') 
+  ? "You win! Live long and prosper!" : "You lose...";
+
+  const result = getResult(playerChoice, computerChoice);
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  if (result === "You win! Live long and prosper!") {
+    playerScore++;
+  } else if (result === "You lose...") {
+    computerScore++;
+  };
+
+  playerScoreDisplay.textContent = `${playerScore}`;
+  computerScoreDisplay.textContent = `${computerScore}`;
+}
